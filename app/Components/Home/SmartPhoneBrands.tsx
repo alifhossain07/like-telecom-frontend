56628bb0970e9be5bb84218e7268027bca521ffc
Alif <alifhossain56782@gmail.com>
2025-10-25 19:37:54 +0600

    Authentic Product Section Done

--- app/Components/Home/SmartPhoneBrands.tsx
+++ app/Components/Home/SmartPhoneBrands.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 const SmartPhoneBrands = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const brands = [
+    { src: "/images/iphoneLogo.png", alt: "iPhone" },
+    { src: "/images/samsunglogo.png", alt: "Samsung" },
+    { src: "/images/oppologo.png", alt: "Oppo" },
+    { src: "/images/realmelogo.png", alt: "Realme" },
+    { src: "/images/huaweilogo.png", alt: "Huawei" },
+    { src: "/images/onepluslogo.png", alt: "OnePlus" },
+    { src: "/images/realmelogo.png", alt: "Realme" },
+    { src: "/images/samsunglogo.png", alt: "Samsung" },
+  ];
+
+  return (
+    <div className="w-10/12 p-10 flex mx-auto items-center bg-gray-200 rounded-2xl">
+      <div className=" py-10 w-1/2 px-4">
+        <h1 className="text-2xl md:text-3xl w-8/12 font-bold text-gray-800 mb-4">
+          Discover The Latest Smartphones From Leading Brands
+        </h1>
+        <p className="text-gray-600  mb-6 leading-relaxed">
+          Stay Connected With Innovation! Explore Our Newest Collection of
+          Smartphones From Top Global Brands — Built For Performance, Style,
+          And Smart Living. Find the Perfect Match For Your Lifestyle and
+          Experience Technology That Moves With You.
+        </p>
+        <button className="bg-orange-600 hover:bg-orange-700 text-white px-6 py-2 flex items-center rounded-3xl font-medium transition">
+          Shop The New Arrivals Today{" "}
+          <FiChevronRight className="text-white text-lg ml-1" />
+        </button>
+      </div>
+
+      {/* Brand Logos */}
+      <div className="grid grid-cols-2 items-center w-1/2 sm:grid-cols-3 md:grid-cols-4 gap-6 px-4 ">
+        {brands.map((brand, index) => (
+          <div
+            key={index}
+            className="bg-white rounded-xl flex items-center justify-center p-6 hover:shadow-lg transition cursor-pointer"
+          >
+            <Image
+              src={brand.src}
+              alt={brand.alt}
+              width={150}
+              height={100}
+              className="object-contain w-auto h-16 md:h-20"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SmartPhoneBrands;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Authentic from "./Components/Home/Authentic";
 import BannerHighlights from "./Components/Home/BannerHighlights";
 import DisplayPhones from "./Components/Home/DisplayPhones";
 import FlashSale from "./Components/Home/FlashSale";
 import Hero from "./Components/Home/Hero";
 import NewArrival from "./Components/Home/NewArrival";
 import ShopFavTech from "./Components/Home/ShopFavTech";
+import SmartPhoneBrands from "./Components/Home/SmartPhoneBrands";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <BannerHighlights />
       <NewArrival />
       <ShopFavTech/>
+      <SmartPhoneBrands/>
+      <Authentic />
     </div>
   );
 }
